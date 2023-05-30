2a88c0a129f7178c91997f942ac4d857e9217bea
himanshusharma8583 <himanshusharma8583@gmail.com>
2023-05-31 02:20:30 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
+
+
+const app = express();
+const server =  http.createServer(app)
+const io = socketio(server)
+
+
+//Set static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+// Run when client connects
+io.on('connection', socket=>{
+ 
+
+//Welcome Current User
+socket.emit("message", "Welcome to CHat APP");
+
+//BroadCast when a User Connects
+socket.broadcast.emit('message','A user has joined the chat');
+
+
+//Runs when client disconnects
+
+socket.on('disconnect', () =>{
+    io.emit('message', 'A user has left the chat');
+})
+
+//Listen for chatMessage
+socket.on('chatMessage', msg =>{
+    io.emit('message', msg);
+})
+
+
+})
+
+
+const PORT  = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
