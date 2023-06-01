bc894685db8a2a06881db6e4c89b3c1981cf98ba
himanshusharma8583 <himanshusharma8583@gmail.com>
2023-06-02 02:12:42 +0530

    final update

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,29 @@
 const chatForm = document.getElementById('chat-form')
 const chatMessages = document.querySelector('.chat-messages')
+const roomName = document.getElementById('room-name')
+const userList = document.getElementById('users')
+
+//Get username and room from URL
+
+const {username, room} = Qs.parse(location.search,{
+  ignoreQueryPrefix: true
+});
+console.log(username, room);
 
 const socket = io();
+
+//Join Chatroom
+socket.emit('joinRoom', { username, room})
+
+//Get Room and Users
+socket.on('roomUsers',({room,users})=>{
+  outputRoomName(room);
+  outputUsers(users);
+  
+
+})
+
+
 
 //Message From Server
 socket.on('message',message=>{
@@ -41,4 +63,20 @@ function outputMessage(message){
   ${message.text}
   </p>`
   document.querySelector(".chat-messages").appendChild(div);
+}
+
+
+
+//Add room name to DOM
+
+function outputRoomName(room){
+roomName.innerText = room;
+
+}
+
+// Add users to DOM
+function outputUsers(users){
+  userList.innerHTML = `
+  ${users.map(user=>`<li>${user.username}</li>`).join('')}
+  `;
 }
--- server.js
+++ server.js
@@ -1,45 +1,74 @@
-const express = require('express')
-const path = require('path')
-const http = require('http')
-const socketio = require('socket.io')
-const formatMessage = require('./utils/messages')
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
 
 const app = express();
-const server =  http.createServer(app)
-const io = socketio(server)
-
+const server = http.createServer(app);
+const io = socketio(server);
 
 //Set static folder
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const botName = 'ChatBot';
+const botName = "ChatBot";
 
 // Run when client connects
-io.on('connection', socket=>{
- 
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
-//Welcome Current User
-socket.emit("message", formatMessage(botName,'Welcome To Chat App'));
+    socket.join(user.room);
 
-//BroadCast when a User Connects
-socket.broadcast.emit("message", formatMessage(botName, "A User has joined the chat"));
+    //Welcome Current User
+    socket.emit("message", formatMessage(botName, "Welcome To Chat App"));
 
+    //BroadCast when a User Connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+    //Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
 
-//Runs when client disconnects
+  //Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
 
-socket.on('disconnect', () =>{
-    io.emit("message", formatMessage(botName, "A user has left the chat"));
-})
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
 
-//Listen for chatMessage
-socket.on('chatMessage', msg =>{
-    io.emit("message", formatMessage('USER', msg));
-})
+  //Runs when client disconnects
 
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+      //Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+   
+  });
+});
 
-})
-
-
-const PORT  = 3000 || process.env.PORT;
+const PORT = 3000 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
